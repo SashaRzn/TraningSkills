42b3277403815fcd4ff99c80eb36a9d69eeabac6
Alexander_Bakushin <spyware633@gmail.com>
2017-10-08 02:08:06 +0400

    Добавлено отображение приветствующего текста при открытии страницы по умолчанию. В .cs файлах пробелы заменены на табы.

---
+++ StudySolution/Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return Content("Сайт находится в разработке. ©bacdob");
+		}
+	}
+}
